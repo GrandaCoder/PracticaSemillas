1e2478abb1ef32d0f915da226baa1c25bbe50749
GrandaCoder <67969004+GrandaCoder@users.noreply.github.com>
2021-09-02 15:35:43 -0500

    Funcion de generar número pseudoaleatorios mediante lineal congrente

--- PracticaSemillas/Program.cs
+++ PracticaSemillas/Program.cs
@@ -6,7 +6,29 @@ namespace PracticaSemillas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GeneradorLinealCongruente(5, 5, 13, 7);
         }
+
+        public static void GeneradorLinealCongruente(int x, int a, int c,int m)
+        {
+            Console.WriteLine("Generando números mediante el metodo lineal congruente...");
+
+            //creamos una variable auxiliar que nos guarda el valor inicial de x0 para despues irlo iterando
+            int valorParada = x;
+            int periodo = 0;
+            do
+            {
+                //mostramos en pantalla los valores 
+                Console.WriteLine("X{0} = {1}",periodo,valorParada);
+                periodo++;
+                //aplicamo la formula de generador lineal congruente
+                valorParada = (a * valorParada + c) % m;
+
+                // el valor de parada esta definida hasta que no se repita el valor x0 y el xn
+                //cuando sean iguales quiere decir que termino el perido
+            } while (x!=valorParada);
+            Console.WriteLine("Perido Total {0}:",periodo);
+        }
+
     }
 }
