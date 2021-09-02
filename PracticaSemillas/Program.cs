1b15bf63555a053ef4ffc4f71fda5b67d7b560b2
GrandaCoder <67969004+GrandaCoder@users.noreply.github.com>
2021-09-02 14:54:37 -0500

    Agregar archivos de proyecto.

---
+++ PracticaSemillas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticaSemillas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
