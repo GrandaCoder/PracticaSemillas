7f87a1bfa474ee9f8b6b40e54eabd73f22201caf
GrandaCoder <67969004+GrandaCoder@users.noreply.github.com>
2021-09-02 16:58:47 -0500

    Funcion estandar mínimo agregada

--- PracticaSemillas/Program.cs
+++ PracticaSemillas/Program.cs
@@ -6,7 +6,11 @@ namespace PracticaSemillas
     {
         static void Main(string[] args)
         {
-            GeneradorLinealCongruente(5, 5, 13, 7);
+            // valores adecuados como ejemplo pequeño
+            //GeneradorLinealCongruente(5, 5, 13, 7);
+
+            // Valores inadecuados
+            //GeneradorEstandarMinimo(5, 12, 5, 21);
         }
 
         public static void GeneradorLinealCongruente(int x, int a, int c,int m)
@@ -29,6 +33,34 @@ namespace PracticaSemillas
             } while (x!=valorParada);
             Console.WriteLine("Perido Total {0}:",periodo);
         }
+
+        public static void GeneradorEstandarMinimo(int x, int a, int c, double m)
+        {
+            double valorParada = x;
+            int periodo = 0;
+
+            double q = Math.Floor(m / a);
+            double r = m % a;
+
+            Console.WriteLine("m Máximo = a*q+r "+a*q+r);
+
+            do
+            {
+                Console.WriteLine("X{0} = {1}", periodo, valorParada);
+                periodo++;
+
+                double operacion = a * (valorParada % q) - r*(Math.Floor(valorParada/q));
+                //Console.WriteLine(operacion);
+
+                valorParada = operacion >= 0 ? operacion : (operacion + m);
+
+                if(valorParada < 0) { break; };
+
+            } while (x != valorParada);
+
+        }
+
+        
 
     }
 }
