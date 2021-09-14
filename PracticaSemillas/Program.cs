2f6965bfa5057340e8500cef30349bd5046abfbd
GrandaCoder <67969004+GrandaCoder@users.noreply.github.com>
2021-09-14 18:41:54 -0500

    ChiCuadrado Funcional

--- PracticaSemillas/Program.cs
+++ PracticaSemillas/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PracticaSemillas
 {
@@ -9,12 +10,107 @@ namespace PracticaSemillas
             // valores adecuados como ejemplo pequeño
             //GeneradorLinealCongruente(5, 5, 13, 7);
 
+            int m = 6075;
+            int x = 5;
+            int a = 106;
+            int c = 1283;
+
+            int n = 1000;
+            float chiCritico = 16.92f;
+            //GeneradorLinealCongruente(x, a, c, m);
+
+            
+            var lista =CalcularRn(GeneradorLinealCongruente(x, a, c, m),m);
+
+            float valorChi = ChiCuadrado(lista,n);
+
+            ComprobarHipotesis(valorChi,chiCritico);
+
+            //foreach (var item in lista)
+            //{
+            //    Console.WriteLine(item);
+            //}
             // Valores inadecuados
             //GeneradorEstandarMinimo(5, 12, 5, 21);
         }
 
-        public static void GeneradorLinealCongruente(int x, int a, int c,int m)
+        //n es la muestra que vamos a generar
+        public static float ChiCuadrado(List<float> rnCalculado, int n)
         {
+            float[] rnLimitado = new float[n];
+            //numero de clases 10
+
+            for (int i = 0; i < n; i++)
+            {
+                rnLimitado[i] = rnCalculado[i];
+            }
+
+            int clases = 10;
+            //int[] FO = new int[clases];
+            int[] FO = CalcularFO(rnLimitado);
+            int[] FE = new int[clases];
+            float[] chiCalculado = new float[clases];
+
+            //foreach (var xn in rnLimitado)
+            //{
+            //    FO[(int)(Math.Floor(xn * 10))] += 1;
+            //}
+
+            for (int i = 0; i < clases; i++)
+            {
+                FE[i] = n / clases;
+                chiCalculado[i] = ((float)Math.Pow((FE[i] - FO[i]), 2) / FE[i]);
+            }
+            float chiValor= 0f;
+            foreach (var item in chiCalculado)
+            {
+                //Console.WriteLine("rango {0}",item );
+                chiValor += item;
+            }
+
+            return chiValor;
+        }
+
+        public static int[] CalcularFO(float[] rnLimitado, int clases=10)
+        {
+            int[] FO = new int[clases];
+            foreach (var xn in rnLimitado)
+            {
+                FO[(int)(Math.Floor(xn * 10))] += 1;
+            }
+
+            return FO;
+        }
+
+        public static void ComprobarHipotesis(float DMCalculado,float DMcritico)
+        {
+            if (DMCalculado <= DMcritico)
+            {
+                Console.WriteLine("Se acepta la hipotesis ya que {0} <= {1} \n El generador es bueno en cuanto a uniformidad.",DMCalculado,DMcritico);
+            }
+            else
+            {
+                Console.WriteLine("No aprueba la hipotesis {0} <= {1} \n El generador NO es bueno en cuanto a uniformidad", DMCalculado, DMcritico);
+            }
+        }
+        public static List<float> CalcularRn(List<int> numerosAleatorios, int m)
+        {
+            List<float> rn = new List<float>();
+
+            //Console.WriteLine(numerosAleatorios.Count);
+
+            foreach (var xn in numerosAleatorios)
+            {
+               // Console.WriteLine(xn);
+                rn.Add(xn / (float)m);
+
+            }
+
+            return rn;
+        }
+        public static List<int> GeneradorLinealCongruente(int x, int a, int c,int m)
+        {
+            List<int> numerosAleatorios = new List<int>();
             Console.WriteLine("Generando números mediante el metodo lineal congruente...");
 
             //creamos una variable auxiliar que nos guarda el valor inicial de x0 para despues irlo iterando
@@ -23,15 +119,20 @@ namespace PracticaSemillas
             do
             {
                 //mostramos en pantalla los valores 
-                Console.WriteLine("X{0} = {1}",periodo,valorParada);
+              
                 periodo++;
                 //aplicamo la formula de generador lineal congruente
                 valorParada = (a * valorParada + c) % m;
 
+                numerosAleatorios.Add(valorParada);
                 // el valor de parada esta definida hasta que no se repita el valor x0 y el xn
                 //cuando sean iguales quiere decir que termino el perido
+
+                //Console.WriteLine("X{0} = {1}", periodo, valorParada);
+
             } while (x!=valorParada);
-            Console.WriteLine("Perido Total {0}:",periodo);
+            //Console.WriteLine("Perido Total {0}:", periodo);
+            return numerosAleatorios;
         }
 
         public static void GeneradorEstandarMinimo(int x, int a, int c, double m)
